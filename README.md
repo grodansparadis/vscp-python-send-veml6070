8a29bf266936029fcc8ac9b3e1a82ae5430c5c6f
Ake <akhe@grodansparadis.com>
2021-09-08 21:47:53 +0200

    Added connection to VSCP daemon

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # vscp-python-send-veml6070
 Send UV index reading from a veml6070 to a VSCP daemon 
+
+## credits
+
+This code is built on code made by ControlEverything.com.
+# https://www.controleverything.com/products
+
+## Installation and setup
+
+i2c support must be enabled. Use raspi-config to enable i2c.
+
+To verify your hardware you can use i2ctools. Install with
+
+```
+sudo apt install i2ctools
+```
+
+scan bus with
+
+```
+i2cdetect 1
+```
+
+you will get something like
+
+```
+     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+00:          -- -- -- -- -- -- -- -- -- -- -- -- -- 
+10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+30: -- -- -- -- -- -- -- -- 38 -- -- -- -- -- -- -- 
+40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+70: -- -- -- -- -- -- -- -- 
+
+```
+
+as output.
+
+The _38_ is the address of the VEML6070 UV sensor.
+
+You also need to install smbus support for python. Install with
+
+```
+pip3 install smbus
+```
+
+## usage
+
+```bash
+vscp_send_veml6070.py host user password [guid] [sensorindex] [zone] [subzone]
+```
+
+| Parameter | Description |
+|----------|-------------|
+| **host**     | VSCP host to connect to |
+| **user**     | VSCP user to connect as |
+| **password** | VSCP password |
+| **guid**     | Optional. GUID to use for the sensor. Default to "-", the daemons interface GUID. |
+| **sensorindex** | Optional. Index of the sensor to use. Defaults to zero. |
+| **zone**     | Optional. Zone to use for the sensor. Default to zero. |
+| **subzone**  | Optional. Subzone to use for the sensor. Defaults to zero. |
+
+Typically you would run this script as a cronjob to send data every minute or so.
+
+
+## example
+
+```
+./vscp_send_veml6070.py localhost admin secret - 0 0 0
+```
+
+Send a reading to a VSCP daemon on the local host using the interface GUID and setting sensorindex, zone and subzone all to zero. This is the same as using
+
+```
+./vscp_send_veml6070.py localhost admin secret
+```
+
+where default values are used for the sensorindex, zone and subzone.
+
+---
+
+This file is part of the [vscp project](https://www.vscp.org)
+
+---
+
+Part of the [VSCP (Very Simple Control Protocol) project](https://www.vscp.org)
--- vscp_send_veml6070.py
+++ vscp_send_veml6070.py
@@ -6,8 +6,16 @@
 # This code is designed to work with the VEML6070_I2CS I2C Mini Module available from ControlEverything.com.
 # https://www.controleverything.com/products
  
+# vscp_bh1750.py
+# Adoptions for the VSCP project by Ake Hedman, 2021-09-07
+# https://www.vscp.org
+# https://www.vishay.com/docs/84310/designingveml6070.pdf
+
 import smbus
 import time
+import getpass
+import sys
+import telnetlib
  
 # Get I2C bus
 bus = smbus.SMBus(1)
@@ -20,41 +28,102 @@ VEML6070_CMD_ACK_DISABLE				= 0x00 # Acknowledge Disable
 VEML6070_CMD_ACK_ENABLE					= 0x20 # Acknowledge Enable
 VEML6070_CMD_ACK_THD_102				= 0x00 # Acknowledge threshold 102 Steps
 VEML6070_CMD_ACK_THD_145				= 0x10 # Acknowledge threshold 145 Steps
-VEML6070_CMD_IT_1_2T					= 0x00 # Integration time = 1/2T
-VEML6070_CMD_IT_1T					= 0x04 # Integration time = 1T
-VEML6070_CMD_IT_2T					= 0x08 # Integration time = 2T
-VEML6070_CMD_IT_4T					= 0x0C # Integration time = 4T
-VEML6070_CMD_RESERVED					= 0x02 # Reserved, Set to 1
+VEML6070_CMD_IT_1_2T					  = 0x00 # Integration time = 1/2T
+VEML6070_CMD_IT_1T					    = 0x04 # Integration time = 1T
+VEML6070_CMD_IT_2T					    = 0x08 # Integration time = 2T
+VEML6070_CMD_IT_4T					    = 0x0C # Integration time = 4T
+VEML6070_CMD_RESERVED					  = 0x02 # Reserved, Set to 1
 VEML6070_CMD_SD_DISABLE					= 0x00 # Shut-down Disable
 VEML6070_CMD_SD_ENABLE					= 0x01 # Shut-down Enable
-VEML6070_CMD_READ_LSB					= 0x38 # Read LSB of the data
-VEML6070_CMD_READ_MSB					= 0x39 # Read MSB of the data
+VEML6070_CMD_READ_LSB					  = 0x38 # Read LSB of the data
+VEML6070_CMD_READ_MSB					  = 0x39 # Read MSB of the data
+
+
+# host user password guid sensorindex zone, subzone
+if ( len(sys.argv) < 4 ):
+    sys.exit("Wrong number of parameters - aborting")
+
+host = sys.argv[1]
+user = sys.argv[2]
+password = sys.argv[3]
+
+guid = "-"
+if ( len(sys.argv) > 4 ):
+  guid = sys.argv[4]
+
+unit = 0
+
+sensorindex = 0
+if ( len(sys.argv) > 5 ):
+    sensorindex = sys.argv[5]
+
+zone = 0
+if ( len(sys.argv) > 6 ):
+    zone = sys.argv[6]
+
+subzone = 0
+if ( len(sys.argv) > 7 ):
+    subzone = sys.argv[7]
+
 
 class VEML6070():
-    def __init__(self):
-        self.write_command()
- 
-    def write_command(self):
-        #"""Select the UV light command from the given provided values"""
-        COMMAND_CONFIG = (VEML6070_CMD_ACK_DISABLE | VEML6070_CMD_IT_1_2T | VEML6070_CMD_SD_DISABLE | VEML6070_CMD_RESERVED)
-        bus.write_byte(VEML6070_DEFAULT_ADDRESS, COMMAND_CONFIG)
+  def __init__(self):
+      self.write_command()
 
-    def read_uvlight(self):
-        #"""Read data back VEML6070_CMD_READ_MSB(0x73) and VEML6070_CMD_READ_LSB(0x71), uvlight MSB, uvlight LSB"""
-        data0 = bus.read_byte(VEML6070_CMD_READ_MSB)
-        data1 = bus.read_byte(VEML6070_CMD_READ_LSB)
-        
-        # Convert the data
-        uvlight = data0 * 256 + data1
-        
-        return {'u' : uvlight}
- 
-from veml6070 import VEML6070
-veml6070 = VEML6070()
+  def write_command(self):
+      #"""Select the UV light command from the given provided values"""
+      COMMAND_CONFIG = (VEML6070_CMD_ACK_DISABLE | VEML6070_CMD_IT_1_2T | VEML6070_CMD_SD_DISABLE | VEML6070_CMD_RESERVED)
+      bus.write_byte(VEML6070_DEFAULT_ADDRESS, COMMAND_CONFIG)
 
-while True:
-	light = veml6070.read_uvlight()
-	print("UV Light Level : %d" %(light['u']))
-	print(" *********************************** ")
-	time.sleep(0.5)
+  def read_uvlight(self):
+      #"""Read data back VEML6070_CMD_READ_MSB(0x73) and VEML6070_CMD_READ_LSB(0x71), uvlight MSB, uvlight LSB"""
+      data0 = bus.read_byte(VEML6070_CMD_READ_MSB)
+      data1 = bus.read_byte(VEML6070_CMD_READ_LSB)
+      
+      # Convert the data
+      uvlight = data0 * 256 + data1
+      
+      return {'u' : uvlight}
 
+
+
+def main():
+    
+  from veml6070 import VEML6070
+
+  # Initialize
+  veml6070 = VEML6070() 
+
+  # Connect to VSCP daemon
+  tn = telnetlib.Telnet(host, 9598)
+  tn.read_until("+OK".encode('ascii'),2)
+
+  # Login
+  tn.write("user " .encode('ascii') + user.encode('ascii') + "\n".encode('ascii'))
+  tn.read_until("+OK".encode('ascii'), 2)
+
+  tn.write("pass " .encode('ascii') + password .encode('ascii') + "\n".encode('ascii'))
+  tn.read_until("+OK - Success.".encode('ascii'),2)
+
+  light = veml6070.read_uvlight()
+  print("UV Light Level : %d" %(light['u']))
+
+  event = "3,"                # Priority=normal
+  event += "90,52,"         # Level II measurement (string), Illuminance    
+  event += "0,"               # Use obid of interface
+  event += ","                # DateTime
+  event += "0,"               # Use interface timestamp
+  event += guid + ","         # add GUID to event
+  event += str(zone) + ","
+  event += str(subzone) + ","
+  event += str(light['u'])
+
+  # Send event to server
+  print("event=" + event)
+  tn.write("send " .encode('ascii') + event .encode('ascii') + "\n".encode('ascii'))
+  tn.read_until("+OK - Success.".encode('ascii'),2)
+
+  tn.write("quit\n".encode('ascii'))  
+
+if __name__=="__main__":
+   main()
